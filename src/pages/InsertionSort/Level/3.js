002409d1b774cd61f57a284280dcc87e3775f1e5
krish <krishabotadara@gmail.com>
2024-12-28 20:10:03 +0700

    Fixed deployment error

--- src/pages/InsertionSort/Level/3.js
+++ src/pages/InsertionSort/Level/3.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../app/globals.css";
+import "../../../app/global.css";
 import { useRouter } from "next/router";
 import { 
   LevelModal, 
